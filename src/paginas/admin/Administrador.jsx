ba4c2067249b2221c42f68de2046a6e9bde09287
Lucas Esteves <lucasesteves5304@gmail.com>
2021-12-02 02:41:20 -0300

    att pagina de funcionario

--- src/paginas/admin/Administrador.jsx
+++ src/paginas/admin/Administrador.jsx
@@ -1,12 +1,47 @@
 import React, { useState, useContext, useEffect } from "react";
+import { Container, Content, TextoNav } from "../../components/Header/style";
+import logoImg from '../../assets/poke-store.png'
+import { Link } from "react-router-dom";
 
 function Administrador() {
 
+    const [texto,setTexto] = useState("");
 
-    
-    return ( 
-        <h1> BEM VINDO AO CANAL DE ADMINISTRADOR</h1>
-     );
+    useEffect(() => {
+        logado()
+        }, [])
+
+ function logado() {
+  
+    const logado = localStorage.getItem("username")
+    if(logado) {
+        setTexto("Logout")
+    }
+    else{
+        setTexto("login")
+    }
+}
+    return (
+ 
+        <>
+        <Container>
+            <Content>
+                <img src={logoImg} alt="logo pokemon" />
+                <TextoNav>
+                    <ul>
+                        <li><Link to="/login"> {texto} </Link></li>
+                        <li> <Link to="/admin/produtos">Adicionar Produto</Link></li>
+                        <li><Link to="/">Excluir Produto</Link></li>
+
+                    </ul>
+                </TextoNav>
+            </Content>
+        </Container>
+        
+</>
+
+    );
+
 }
 
 export default Administrador;
