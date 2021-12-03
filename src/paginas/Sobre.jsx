43390b1666e118b52b42be7cd9fad6fcaeebd416
natalino <natalinoesteves2010@gmail.com>
2021-12-03 08:52:58 -0300

    aa

---
+++ src/paginas/Sobre.jsx
@@ -0,0 +1,21 @@
+import React, { } from "react";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import { MainSobre } from "../components/MainSobre";
+
+
+
+
+
+
+export function Sobre() {
+
+    return (
+        <>
+            <Header />
+            <MainSobre />
+            <Footer />
+
+        </>
+    )
+}
