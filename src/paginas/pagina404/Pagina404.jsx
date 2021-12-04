722adbf270fb705f77228689d4ccb8722c0f46b0
Lucas Esteves <lucasesteves5304@gmail.com>
2021-12-04 00:19:37 -0300

    att

--- src/paginas/pagina404/Pagina404.jsx
+++ src/paginas/pagina404/Pagina404.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import imagem from "../../assets/img/Snorlax.svg";
 import { Img } from "react-image";
+import { Container, Typography } from "@material-ui/core";
 
 
 
 const Pagina404 = () => {
   return (
-    <main className="container flex flex--centro flex--coluna">
-     4<Img src={imagem}alt="404" />4
+
+    <Container component="article" maxWidth="sm">
+     <Img src={imagem}alt="404" />  
       <p className="naoencontrado-texto">
         <br/>
         <span>OppS!</span> Um Snorlax selvagem bloqueou seu caminho!
       </p>
-    </main>
+    </Container>
   );
 };
 
